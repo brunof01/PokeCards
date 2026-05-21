fc31926a5a5728ba9e9ad37b0ed49c0afdb6a3f8
brunof01 <bruno.fortes@unochapeco.edu.br>
2026-05-20 21:06:02 -0300

    Implementado novas renderizações do deck e estilização

--- app.js
+++ app.js
@@ -1,56 +1,71 @@
-async function getPokemon(){
+const typeColors = {
+    fire: "from-red-500 to-orange-500",
+    water: "from-blue-500 to-cyan-500",
+    grass: "from-green-500 to-lime-500",
+    electric: "from-yellow-300 to-yellow-600",
+    psychic: "from-pink-500 to-purple-500",
+    ghost: "from-purple-700 to-indigo-900",
+    normal: "from-gray-400 to-gray-600"
+};
+
+async function getPokemon() {
     const id = Math.floor(Math.random() * 151) + 1;
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     const data = await response.json();
-    return{
-        id: data.id,
-        name: data.name,
-        image: data.sprites.front_default
-    };
-} 
+    return data;
+}
 
-document.getElementById('openCard').onclick = async () =>
-{
+document.getElementById('openCard').onclick = async () => {
     const pokemon = await getPokemon();
     renderCard(pokemon);
 }
 
-function renderCard(pokemon){
+function renderCard(pokemon) {
     const card = document.getElementById('card');
-    card.innerHTML = 
-    `
-    <div class ="max-w-md bg-white border border-gray-200 rounded-lg shadow-sm">
-        <img class="rounded-t-lg w-full bg-slate-200" 
-        src ="${pokemon.image}">          
+    const bgColor = typeColors[pokemon.types[0].type.name] || "from-slate-500 to-slate-700";
 
-        <h5 class="mb-2 font-bold text-gray-900 text-2xl tracking-tight capitalize">${pokemon.name}</h5>
-        <p class="mb-3 font-normal text-gray-700">#${pokemon.id}</p>
-        <button id="saveBtn" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bf-blue-700 rounded-lg hover:bg-blue-800">Guardar</button>
+    card.innerHTML =
+    `
+    <div class ="max-w-md bg-gradient-to-br ${bgColor} rounded-lg shadow-md overflow-hidden animate-pulse">
+        <img class="rounded-t-lg w-full bg-slate-200" 
+        src ="${pokemon.sprites.other.showdown.front_default}">
+
+        <div class="p-5">
+            <h5 class="mb-2 font-bold text-gray-900 text-2xl tracking-tight capitalize">${pokemon.name}</h5>
+
+            <p class="mb-3 font-normal text-gray-700">#${pokemon.id}</p>
+
+            <button id="saveBtn" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800">Guardar</button>
+
+        </div>
+        
     </div>
     `;
     document.getElementById('saveBtn').onclick = () => {
-        addToDeck(pokemon.id, pokemon.name, pokemon.image);
+        addToDeck(pokemon);
     };
 }
 
-async function addToDeck(id, name, image){
-    await savePokemon( {id, name, image});
+async function addToDeck(pokemon) {
+    await savePokemon(pokemon);
     await loadDeck();
 }
 
-async function loadDeck(){
-    const result = await db.allDocs({include_docs: true});
+async function loadDeck() {
+    const result = await db.allDocs({ include_docs: true });
+    renderDeck(result.rows);
+}
+
+function renderDeck(pokemons){
     const deck = document.getElementById('deck');
     deck.innerHTML = '';
-    result.rows.forEach(row => {
-        const pokemon = row.doc;
-        deck.innerHTML += 
-        `
-        <div class="card">
-            <img src ="${pokemon.image}">
-            <p>${pokemon.name}</p>
-            <p>lvl: ${pokemon.level}</p>
+    pokemons.forEach(pokemon => {
+        deck.innerHTML += `
+        <div class="bg-white rounded-xl p-4 shadow-lg">
+            <img src ="${pokemon.doc.sprites.other.showdown.front_default}" class="w-full h-32 object-contain">
+            <h3 class="text-black text-xl font-bold capitalize">${pokemon.doc.name}</h3>
+            <p class="text-gray-700"> lvl: ${pokemon.doc.level}</p>
         </div>
-        `;
+        `
     });
 }
--- db.js
+++ db.js
@@ -2,17 +2,12 @@ const db = new PouchDB('deck');
 
 async function savePokemon(pokemon){
     try{
-        const existing = await db.get(String(pokemon.id));
+        const existing = await db.get(pokemon.id.toString());
         existing.level += 1;
         await db.put(existing);
     } catch{
-        await db.put(
-            {
-                _id: String(pokemon.id),
-                name: pokemon.name,
-                image: pokemon.image,
-                level: 1
-            }
-        );
+        pokemon._id = pokemon.id.toString();
+        pokemon.level = 1;
+        await db.put(pokemon);
     }
 }
